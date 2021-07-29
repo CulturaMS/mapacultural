76ffebfac81be2c5133d82b22eab123ac2ecccbb
erleibiazzio <erleibiazzio@gmail.com>
2021-07-28 22:12:54 -0300

    Define o texto e o logo do MS cultura cidadã no ambiente Ref.: #17

---
+++ themes/MatoGrossoSul/layouts/parts/text-home.php
@@ -0,0 +1,17 @@
+<h3>MS Cultura Cidadã</h3>
+<p>O Programa “MS Cultura Cidadã, instituído pela Lei Estadual 5.688, de 7 de julho de 2021 e regulamentado pelo Decreto Nº 15.728, de 14 de julho de 2021, 
+integra o pacote de apoio do governo estadual “Retomada MS”, e prevê o uso de até R$ 3.230.000,00 (três milhões, duzentos e trinta mil reais), para concessão
+de apoio financeiro emergencial aos trabalhadores da cultura atingidos por restrições econômicas durante a pandemia da Covid-19.</p>
+
+<p>
+O apoio financeiro vai possibilitar que cerca de 1800 trabalhadores da cultura sul-mato-grossense, entre artistas, contadores de histórias, 
+produtores, técnicos, curadores, oficineiros, professores de escolas de artes e capoeira, designers de moda, entre outros profissionais da cultura estadual, 
+tenham acesso ao apoio financeiro emergencial de R$ 1.800,00 (mil e oitocentos reais), a ser pago em 3 (três) parcelas mensais  no valor de R$ 600,00 
+(seiscentos reais) cada, limitado a um membro por  unidade familiar.
+
+</p>
+
+<div style="text-align: center;">
+    <h3>Inscrições em breve !</h3>
+    <br><br>
+</div>
