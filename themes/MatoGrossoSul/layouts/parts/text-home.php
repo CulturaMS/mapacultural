ad276b5005ad0b2e6681356a163052aa13b4e796
erleibiazzio <erleibiazzio@gmail.com>
2021-07-30 16:26:30 -0300

    Corrige erros no texto MS Cultura Cidadã da home Ref.: #17

--- themes/MatoGrossoSul/layouts/parts/text-home.php
+++ themes/MatoGrossoSul/layouts/parts/text-home.php
@@ -1,12 +1,14 @@
-<p>O Programa “MS Cultura Cidadã, instituído pela Lei Estadual 5.688, de 7 de julho de 2021 e regulamentado pelo Decreto Nº 15.728, de 14 de julho de 2021, 
-integra o pacote de apoio do governo estadual “Retomada MS”, e prevê o uso de até R$ 3.230.000,00 (três milhões, duzentos e trinta mil reais), para concessão
-de apoio financeiro emergencial aos trabalhadores da cultura atingidos por restrições econômicas durante a pandemia da Covid-19.</p>
+<p>O Programa “MS Cultura Cidadã”, instituído pela Lei nº 5.688, de 7 de julho de 2021 e regulamentado pelo Decreto nº 15.728,
+de 14 de julho de 2021, integra o pacote de apoio do governo estadual “Retomada MS”, e prevê o uso de até R$ 3.230.000,00 
+(três milhões, duzentos e trinta mil reais), para concessão de apoio financeiro emergencial aos trabalhadores da cultura atingidos por 
+restrições econômicas durante a pandemia da Covid-19.
+</p>
 
 <p>
-O Programa possibilitará que cerca de 1800 trabalhadores da cultura sul-mato-grossense, dentre artistas, contadores de histórias, produtores, técnicos, curadores, oficineiros, professores de escolas de artes e capoeira, designers de moda, dentre outros profissionais da cultura estadual, 
-tenham acesso ao apoio financeiro emergencial de R$ 1.800,00 (mil e oitocentos reais), a ser pago em 3 (três) parcelas mensais  no valor de R$ 600,00 
-(seiscentos reais) cada, limitado a um membro por  unidade familiar.
-
+O Programa possibilitará que cerca de 1800 trabalhadores da cultura sul-mato-grossense, dentre artistas, 
+contadores de histórias, produtores, técnicos, curadores, oficineiros, professores de escolas de artes e capoeira, 
+designers de moda, dentre outros profissionais da cultura estadual, tenham acesso ao apoio financeiro emergencial de R$ 1.800,00 
+(mil e oitocentos reais), a ser pago em 3 (três) parcelas mensais  no valor de R$ 600,00 (seiscentos reais) cada.
 </p>
 
 <div style="text-align: center;">
