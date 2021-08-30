07962fd0ca27a62220a94c0cf6a1793e6eec659b
erleibiazzio <erleibiazzio@gmail.com>
2021-08-30 01:23:55 -0300

    só exibe o botão do auxilio MS Cultura cidadã se método isStartStreamLined retorna verdadeiro ou botão esta setado para exbir nas configurações

--- themes/MatoGrossoSul/layouts/parts/text-home.php
+++ themes/MatoGrossoSul/layouts/parts/text-home.php
@@ -9,8 +9,8 @@ $app = App::i();
 
 <div style="text-align: center;">
     <p><?=$text_info_link_documentation?> <strong><a style="color:#fff" target="_blank" href="<?=$link_documentation?>"><?=$text_link_documentation?></a></strong>.</p>
-    <?php if($enabled_button){ ?>
-    <a class="btn btn-accent btn-large" href="<?=$link_button?>"><?=$text_button?></a>
+    <?php if($enabled_button || $isStartStreamLined){ ?>
+        <a class="btn btn-accent btn-large" href="<?=$link_button?>"><?=$text_button?></a>
     <?php }else{ ?>
         <h3><?=$text_button_disabled?></h3>
     <?php } ?>
