357a0267efc31360c2d45959d2139bf7875857dd
erleibiazzio <erleibiazzio@gmail.com>
2021-09-13 17:51:23 -0300

    Remove pontuação desnecessária

--- themes/MatoGrossoSul/layouts/parts/text-home.php
+++ themes/MatoGrossoSul/layouts/parts/text-home.php
@@ -8,7 +8,7 @@ $app = App::i();
 <?=$text?>
 
 <div style="text-align: center;">
-    <p><?=$text_info_link_documentation?> <strong><a style="color:#fff" target="_blank" href="<?=$link_documentation?>"><?=$text_link_documentation?></a></strong>.</p>
+    <p><?=$text_info_link_documentation?> <strong><a style="color:#fff" target="_blank" href="<?=$link_documentation?>"><?=$text_link_documentation?></a></strong></p>
     <?php if($enabled_button || $isStartStreamLined){ ?>
         <a class="btn btn-accent btn-large" href="<?=$link_button?>"><?=$text_button?></a>
     <?php }else{ ?>
