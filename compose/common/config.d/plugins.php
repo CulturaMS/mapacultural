b1592a5639d4735b960fd5b177527be67ec4f504
erleibiazzio <erleibiazzio@gmail.com>
2021-11-10 12:48:31 -0300

    instala plugin LocationPatch

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -4,7 +4,13 @@ $config_plugin = [
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
-        
+        "LocationPatch" => [
+            "namespace" => "LocationPatch",
+            "config" => [
+                "enable" => env("LOCATION_PATCH_ENABLE", true),
+                "cutoff" => env("LOCATION_PATCH_CUTOFF", "19800101000001"),
+            ],
+        ],        
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'MapasBlame' => [
             'namespace' => 'MapasBlame',
